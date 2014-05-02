40759e6ae1fde173da9ccde128a233bcc9cdf665
Bjørn Trondsen <bjorn@arctodus.no>
2014-05-02 19:04:07 +0200

    accept collector argument

--- lib/arel/visitors/sqlanywhere.rb
+++ lib/arel/visitors/sqlanywhere.rb
@@ -3,7 +3,7 @@ module Arel
     class SQLAnywhere < Arel::Visitors::ToSql
       private
 
-      def visit_Arel_Nodes_SelectStatement o
+      def visit_Arel_Nodes_SelectStatement(o, collector)
         using_distinct = o.cores.any? do |core|
           core.set_quantifier.class == Arel::Nodes::Distinct
         end
@@ -23,29 +23,29 @@ module Arel
           (visit(o.limit) if o.limit),
           (visit(Arel::Nodes::Limit.new(2147483647)) if o.limit == nil and o.offset),
           (visit(o.offset) if o.offset),
-          o.cores.map { |x| visit_Arel_Nodes_SelectCore x }.join,
+          o.cores.map { |x| visit_Arel_Nodes_SelectCore(x, collector) }.join,
           ("ORDER BY #{o.orders.map { |x| visit x }.join(', ')}" unless o.orders.empty?),
           (visit(o.lock) if o.lock),
         ].compact.join ' '
       end
       
-      def visit_Arel_Nodes_SelectCore o
+      def visit_Arel_Nodes_SelectCore(o, collector)
         super.sub(/^SELECT(\s+DISTINCT)?\s*/, '')
       end
 
-      def visit_Arel_Nodes_Offset o
+      def visit_Arel_Nodes_Offset(o)
         "START AT #{visit(o.expr) + 1}"
       end
 
-      def visit_Arel_Nodes_Limit o
+      def visit_Arel_Nodes_Limit(o)
         "TOP #{visit o.expr}"
       end
       
-      def visit_Arel_Nodes_True o
+      def visit_Arel_Nodes_True(o)
         "1=1"
       end
       
-      def visit_Arel_Nodes_False o
+      def visit_Arel_Nodes_False(o)
         "1=0"
       end
 
